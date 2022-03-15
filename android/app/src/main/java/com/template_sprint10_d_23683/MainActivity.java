f72167518a75c938778d1e707df7353687ebc1d2
Crowdbotics <engineering@crowdbotics.com>
2022-03-15 06:23:34 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/template_sprint10_d_23683/MainActivity.java
@@ -0,0 +1,15 @@
+package com.template_sprint10_d_23683;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "template_sprint10_d_23683";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "template_sprint10_d_23683",
+  url: "https://template_sprint10_d_23683.botics.co",
+  api: "https://template_sprint10_d_23683.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
